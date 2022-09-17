0ace34ef18f728c18a49da8f9db81bea34ea1410
VO!D <113063012+spiral-999@users.noreply.github.com>
2022-09-17 18:13:15 -0300

    Update Carro.cpp

--- POO/Veiculo/Carro.cpp
+++ POO/Veiculo/Carro.cpp
@@ -5,14 +5,18 @@
 using namespace std;
 
 int main(){
+    cout << "\nBEM VINDO AO SEU VEICULO " << endl << endl;
+    cout << "digite abaixo os dados sobre ele\n" << endl;
+    
     string s;
     int p;
     carropasseio carrin;
-    cout << "Digite o Peso do seu carro:" << endl;
+    cout << "Qual o peso do seu carro? " << endl;
     cin >> p;
     carrin.setpeso(p);
-    cout << endl << "Digite a velocidade máxima do seu carro:" << endl;
+    cout << endl << "Agora informe a velocidade maxima do seu carro :" << endl;
     cin >> p;
+    cout << endl;
     carrin.setvelma(p);
     carrin.impr();
 }
